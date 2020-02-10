0c8bfb128c2f28ceb713e44c9bb0cd13fdc9f9a6
Jing Guo <jing@prestolabs.io>
2020-02-10 15:44:26 +0800

    udpated

--- utils/compare_output.py
+++ utils/compare_output.py
@@ -2,6 +2,9 @@ import os
 import sys 
 import subprocess
 
+# Perform a file-to-file comparison between all the files in
+# the two input directories
+
 if len(sys.argv) != 3:
     print("Usage: python compare_output.py dir1 dir2")
     exit(1)
@@ -19,10 +22,13 @@ for current_root, dirs, files in os.walk(dir2):
     for f in files:
         dir2_files.append(os.path.join(current_root, f))
 
-if len(dir1_files) != len(dir2_files):
-    print("Different number of files")
+num_of_files_dir1 = len(dir1_files)
+num_of_files_dir2 = len(dir2_files)
+
+if num_of_files_dir1 != num_of_files_dir2:
+    print("Different number of files (" + str(num_of_files_dir1) + " vs " + str(num_of_files_dir2) + ")")
 else:
-    print("Same number of files")
+    print("Same number of files (" + str(num_of_files_dir1) + ")")
 
 for f1, f2 in zip(dir1_files, dir2_files):
     res = subprocess.run(["diff", f1, f2])
@@ -31,4 +37,4 @@ for f1, f2 in zip(dir1_files, dir2_files):
     except subprocess.CalledProcessError as err:
         sys.exit(err.cmd)
 
-print("Outputs are identical")
+print("All files are identical")
